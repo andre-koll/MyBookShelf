a19fd8f05d6c5bd4e055e202573befefbf42a4ae
Andre Koll <akoll@web.de>
2026-06-29 06:04:09 +0200

    hope it worx now

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,3 @@
 import { createAuthClient } from 'better-auth/react'
 
 export const { signIn, signUp, signOut, useSession, getSession } = createAuthClient()
-
-// console.log('server auth-client');
-
-// const session = await getSession({ req });
